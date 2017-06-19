2c97d1fa2874090e49111532e79df9c07edb918e
François POUTHIER <francois.pouthier@gmail.com>
2017-06-19 22:33:30 +0200

    stats graph

--- src/components/PokemonStats.js
+++ src/components/PokemonStats.js
@@ -3,13 +3,23 @@ import React from 'react';
 export default function PokemonStats(props) {
     let stats = props.stats.reverse();
 
+    function getWidth(width) {
+        return {
+            width: width
+        }
+    }
+
     return (
         <div className="pokemon-stats">
             <h3>Stats</h3>
 
             <ul className="stats-list">
                 {stats.map(stat => (
-                    <li className={`stat-${stat.stat.name}`} key={`stat-${stat.stat.name}`}>{stat.stat.name}: {stat.base_stat}</li>
+                    <li className={`stat-${stat.stat.name}`} key={`stat-${stat.stat.name}`}>
+                        <span className="stat-label">{stat.stat.name}</span>
+                        <span className="stat-value">{stat.base_stat}</span>
+                        <span className="stat-progress-bar" style={getWidth(stat.base_stat)} />
+                    </li>
                 ))}
             </ul>
         </div>
